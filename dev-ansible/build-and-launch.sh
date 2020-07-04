b4feebdd1092ce40945850d147447f8301ab2836
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2020-07-04 19:52:34 +0200

    Update.

--- dev-ansible/build-and-launch.sh
+++ dev-ansible/build-and-launch.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 
-AWS_REGION="eu-central-1"
-
 ARTIFACT=`packer build -machine-readable packer.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 echo "packer output:"
 cat packer.json
@@ -13,9 +11,10 @@ echo "AMI ID: ${AMI_ID}"
 echo "writing amivar.tf and uploading it to s3"
 echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
 
+terraform init && terraform apply -auto-approve
+
+AWS_REGION="eu-central-1"
 S3_BUCKET=`aws s3 ls --region $AWS_REGION | grep terraform-state |tail -n1 |cut -d ' ' -f3`
 aws s3 cp amivar.tf s3://${S3_BUCKET}/amivar.tf --region $AWS_REGION
-
-terraform init && terraform apply -auto-approve
 sed -i 's/#//g' backend.tf
 terraform init
