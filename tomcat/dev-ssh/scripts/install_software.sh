e46624808fde5f02570515fc6f2799a237e37a53
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2020-07-07 02:59:05 +0200

    Update.

--- tomcat/dev-ssh/scripts/install_software.sh
+++ tomcat/dev-ssh/scripts/install_software.sh
@@ -9,7 +9,16 @@ done
 # install tomcat
 echo "127.0.0.1 $(hostname)" >> /etc/hosts
 apt-get update
-apt-get install -y tomcat
-
-# make sure tomcat is started
-service tomcat start
+sudo apt-get install -y default-jdk
+groupadd tomcat
+useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+wget http://apache.mirror.digitalpacific.com.au/tomcat/tomcat-8/v8.5.56/bin/apache-tomcat-8.5.56.tar.gz
+tar -xzvf apache-tomcat-8.5.56.tar.gz
+mv apache-tomcat-8.5.56 /opt/tomcat
+chgrp -R tomcat /opt/tomcat
+chown -R tomcat /opt/tomcat
+chmod -R 755 /opt/tomcat
+cp ../config/tomcat.service /etc/systemd/system/tomcat.service
+systemctl daemon-reload
+systemctl start tomcat
+systemctl enable tomcat
